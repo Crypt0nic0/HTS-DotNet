b85b88e05fd2373e3eabd283edbc915faf4ceb27
Crypt0nic0 <n.greneche45@gmail.com>
2023-10-05 16:42:04 +0200

    Exercice 2 - Interpolation de chaine

---
+++ Exercice2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Saisir votre prénom : ");
+string prenom = Console.ReadLine();
+Console.WriteLine("Saisir votre âge : ");
+string age = Console.ReadLine();
+Console.WriteLine($"Prénom de l'utilisateur : {prenom} - Age de l'utilisateur : {age} ans");
