c2c9ce836f5a218dfe875888a8f9661fd6b90cc5
Crypt0nic0 <n.greneche45@gmail.com>
2023-10-09 10:56:25 +0200

    Exercice2 Updated

--- Exercice2/Program.cs
+++ Exercice2/Program.cs
@@ -4,3 +4,4 @@ string prenom = Console.ReadLine();
 Console.WriteLine("Saisir votre âge : ");
 string age = Console.ReadLine();
 Console.WriteLine($"Prénom de l'utilisateur : {prenom} - Age de l'utilisateur : {age} ans");
+Console.WriteLine("Bonjour " + prenom + ", vous avez " + age + " ans.");
