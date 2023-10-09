601a76fff1e8f6da7df23d2da6ab7a5fe66d6633
Crypt0nic0 <n.greneche45@gmail.com>
2023-10-09 11:44:34 +0200

    Exercice Nombre Mystere created

---
+++ NombreMystere/Program.cs
@@ -0,0 +1,23 @@
+﻿//============== JEU NOMBRE MYSTERE ==============//
+
+const int nbmyst = 5;
+
+Console.WriteLine("Bonjour et bienvenu dans le jeu du Nombre Mystère, quel est votre nom ?");
+string nom = Console.ReadLine();
+
+Console.WriteLine($"Bienvenu {nom}");
+Console.WriteLine("Saisir un nombre : ");
+int nb = int.Parse(Console.ReadLine());
+
+if (nb == nbmyst)
+{
+    Console.WriteLine("Bravo vous avez trouvé !");
+}
+else if (nb > nbmyst)
+{
+    Console.WriteLine("Perdu, vous êtes trop haut =(");
+}
+else
+{
+    Console.WriteLine("Perdu, vous êtes trop bas =(");
+}
