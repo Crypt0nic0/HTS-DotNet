cbab9667821e1964b28e655caeee9cf8bcbe71f8
Crypt0nic0 <n.greneche45@gmail.com>
2023-10-09 11:09:44 +0200

    Exercice2 Updated : string / int conversion

--- Exercice2/Program.cs
+++ Exercice2/Program.cs
@@ -1,7 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+// VERSION 1
+/*
 Console.WriteLine("Saisir votre prénom : ");
 string prenom = Console.ReadLine();
 Console.WriteLine("Saisir votre âge : ");
 string age = Console.ReadLine();
 Console.WriteLine($"Prénom de l'utilisateur : {prenom} - Age de l'utilisateur : {age} ans");
 Console.WriteLine("Bonjour " + prenom + ", vous avez " + age + " ans.");
+*/
+
+// VERSION 2
+Console.WriteLine("Saisir votre prénom : ");
+string prenom = Console.ReadLine();
+Console.WriteLine("Saisir votre âge : ");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine($"Bonjour {prenom}, vous avez {age} ans.");
