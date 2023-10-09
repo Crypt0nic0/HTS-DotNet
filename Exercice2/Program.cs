db459c2f16f917b557766a6992f1d2bc5533f3a0
Crypt0nic0 <n.greneche45@gmail.com>
2023-10-09 11:23:42 +0200

    Exercice 2 Updated - Opérateurs de comparaison

--- Exercice2/Program.cs
+++ Exercice2/Program.cs
@@ -11,8 +11,17 @@ Console.WriteLine("Bonjour " + prenom + ", vous avez " + age + " ans.");
 */
 
 // VERSION 2
+const int maj = 18;
 Console.WriteLine("Saisir votre prénom : ");
 string prenom = Console.ReadLine();
 Console.WriteLine("Saisir votre âge : ");
 int age = int.Parse(Console.ReadLine());
 Console.WriteLine($"Bonjour {prenom}, vous avez {age} ans.");
+if (age >= maj)
+{
+    Console.WriteLine("Vous êtes majeur.");
+}
+else
+{
+    Console.WriteLine("Vous êtes mineur.");
+}
