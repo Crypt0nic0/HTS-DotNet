93478a739aa7ca59fc44609098737662caaf7b25
Crypt0nic0 <n.greneche45@gmail.com>
2023-10-09 12:45:06 +0200

    Nombre Mystere - Ajout boucle while

--- NombreMystere/Program.cs
+++ NombreMystere/Program.cs
@@ -1,23 +1,29 @@
 ﻿//============== JEU NOMBRE MYSTERE ==============//
 
 const int nbmyst = 5;
+bool trouv = false;
 
 Console.WriteLine("Bonjour et bienvenu dans le jeu du Nombre Mystère, quel est votre nom ?");
 string nom = Console.ReadLine();
 
 Console.WriteLine($"Bienvenu {nom}");
-Console.WriteLine("Saisir un nombre : ");
-int nb = int.Parse(Console.ReadLine());
 
-if (nb == nbmyst)
+while (!trouv)
 {
-    Console.WriteLine("Bravo vous avez trouvé !");
-}
-else if (nb > nbmyst)
-{
-    Console.WriteLine("Perdu, vous êtes trop haut =(");
-}
-else
-{
-    Console.WriteLine("Perdu, vous êtes trop bas =(");
+    Console.WriteLine("Saisir un nombre : ");
+    int nb = int.Parse(Console.ReadLine());
+
+    if (nb == nbmyst)
+    {
+        Console.WriteLine("Bravo vous avez trouvé !");
+        trouv = true;
+    }
+    else if (nb > nbmyst)
+    {
+        Console.WriteLine("Perdu, vous êtes trop haut =(");
+    }
+    else
+    {
+        Console.WriteLine("Perdu, vous êtes trop bas =(");
+    }
 }
