788bfda02d6bec194b1f742861672ecdbb3e02cf
Crypt0nic0 <n.greneche45@gmail.com>
2023-10-09 12:15:49 +0200

    Exercice 3 - Boucles for

---
+++ Exercice3/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] tabNb = new int[3];
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("Saisir un nombre : ");
+    tabNb[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine($"Ligne {i} : {tabNb[i]}");
+}
