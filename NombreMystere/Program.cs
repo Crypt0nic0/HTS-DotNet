ff1a6b85c439e820b9fe3d490320bf7c8404bdb2
Crypt0nic0 <n.greneche45@gmail.com>
2023-10-09 13:13:34 +0200

    Nombre Mystere - Ajout historique saisie

--- NombreMystere/Program.cs
+++ NombreMystere/Program.cs
@@ -2,6 +2,9 @@
 
 const int nbmyst = 5;
 bool trouv = false;
+string indic = "";
+
+List<int> histoNb = new List<int>();
 
 Console.WriteLine("Bonjour et bienvenu dans le jeu du Nombre Mystère, quel est votre nom ?");
 string nom = Console.ReadLine();
@@ -10,8 +13,17 @@ Console.WriteLine($"Bienvenu {nom}");
 
 while (!trouv)
 {
+    Console.Clear();
+    Console.WriteLine(indic);
+    Console.WriteLine("Liste de nombres déjà joués : ");
+    foreach (int n in histoNb)
+    {
+        Console.Write($" {n}");
+    }
+    Console.WriteLine("");
     Console.WriteLine("Saisir un nombre : ");
     int nb = int.Parse(Console.ReadLine());
+    histoNb.Add(nb);
 
     if (nb == nbmyst)
     {
@@ -20,10 +32,10 @@ while (!trouv)
     }
     else if (nb > nbmyst)
     {
-        Console.WriteLine("Perdu, vous êtes trop haut =(");
+        indic = "Perdu, vous êtes trop haut =(";
     }
     else
     {
-        Console.WriteLine("Perdu, vous êtes trop bas =(");
+        indic = "Perdu, vous êtes trop bas =(";
     }
 }
